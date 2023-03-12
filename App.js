b08aea1179d9d86c8e011a780d4c24d286c0e8a7
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2023-03-12 22:07:26 +0100

    everything in app.js

--- App.js
+++ App.js
@@ -8,23 +8,84 @@ import {
   SafeAreaView,
   ActivityIndicator,
 } from "react-native";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+import * as Device from "expo-device";
 
 import messagesApi from "./app/api/messages";
-import useNotification from "./app/hooks/useNotification";
+import expoPushTokensApi from "./app/api/expoPushTokens";
 import MessagesList from "./app/components/messages/MessagesList";
 import MessageModal from "./app/components/messages/MessageModal";
 import Statusbar from "./app/components/Statusbar";
 
 export default function App() {
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const [expoPushToken, setExpoPushToken] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedMessage, setSelectedMessage] = useState(null);
   const [messages, setMessages] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [responseStatus, setResponseStatus] = useState(null);
 
-  const registerStatus = useNotification((notification) => loadMessages());
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      try {
+        const { status: existingStatus } =
+          await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+        }
+        if (finalStatus !== "granted") {
+          alert("Failed to get push token for push notification!");
+          return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        setExpoPushToken(token);
+        sendToken(expoPushToken);
+      } catch (error) {
+        alert(error);
+      }
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+    return token;
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+
+    //ON RECEIVING NOTIFICATION
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener(() => {});
+
+    //ON TOUCHING NOTIFICATION
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener(loadMessages);
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   function showMessageModal(id) {
     let newSelectedItem = messages.find((msg) => msg._id === id);
@@ -45,6 +106,15 @@ export default function App() {
       setMessages(orginalMessages);
     }
     setModalVisible(false);
+  };
+
+  const sendToken = async (token) => {
+    setLoading(true);
+    const response = await expoPushTokensApi.register(token);
+    //console.log(response.data);
+    setLoading(false);
+
+    setResponseStatus(response.status);
   };
 
   const loadMessages = async () => {
@@ -103,13 +173,18 @@ export default function App() {
                   </View>
                 </View>
               ) : (
-                <MessagesList
-                  messages={messages}
-                  onPressItem={showMessageModal}
-                ></MessagesList>
+                !loading && (
+                  <MessagesList
+                    messages={messages}
+                    onPressItem={showMessageModal}
+                  ></MessagesList>
+                )
               )}
             </View>
-            <Statusbar onPress={() => {}} status={registerStatus}></Statusbar>
+            <Statusbar
+              onPress={registerForPushNotificationsAsync}
+              status={responseStatus}
+            ></Statusbar>
           </SafeAreaView>
         </ImageBackground>
       </LinearGradient>
--- app/components/Statusbar.js
+++ app/components/Statusbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Pressable, Text } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 function Statusbar({ onPress, status }) {
